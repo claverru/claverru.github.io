4cd23f4e89d65e1cbe3012b53b0628fd27a843a3
Claudio Verdú <claverru@gmail.com>
2022-12-10 18:52:56 +0100

    feat(pages): Make it for pages.

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+
+const dev = "production" === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +11,13 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs'
+		}),
+		paths: {
+			base: dev ? '' : '',
+		},
 	}
 };
 
